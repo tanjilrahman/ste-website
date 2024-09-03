9e6d99c22ea62d5650bcc9b91f750c1430a9bd91
Tanjil Rahman <tanjil.rahman739@gmail.com>
2024-09-03 21:35:29 +0600

    netlify form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -46,6 +46,35 @@ const Contact = () => {
         {/* <div className="text">
           <h1>Let's make your fashion brand shine</h1>
         </div> */}
+        <form name="contact" method="POST" netlify>
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Role:{" "}
+              <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+              </select>
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message"></textarea>
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
 
         <div className="form">
           <form name="Sanjar Tex & Expo contact form" method="POST" netlify>
